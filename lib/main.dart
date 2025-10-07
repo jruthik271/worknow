2eb19583a7b4d294cb7bfaf35d926e63a178ff11
jruthik271 <jruthik271@gmail.com>
2025-10-07 11:17:30 +0530

    Device_preview

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,18 @@
 import 'dart:async';
 
 import 'package:device_preview/device_preview.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:worknow/login_screen.dart';
 
 void main() {
-  runApp(DevicePreview(builder: (context) => MyApp()));
+   if(kIsWeb){
+    return runApp(DevicePreview(builder: (context)=>MyApp()));
+
+   }
+   else{
+    runApp(MyApp());
+   }
 }
 
 class MyApp extends StatelessWidget {
